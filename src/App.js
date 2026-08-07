8750d3f9a6f885cde393cc990338711874ef441e
chandru90 <iochandru90@gmail.com>
2026-08-06 23:59:24 -0700

    updated

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+
+
+import { useState } from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
+
+import FoodStory from "./Foodstory.js";
+import en from "./i18n/locales/en/translaton.json";
+import ar from "./i18n/locales/ar/translaton.json";
+import ta from "./i18n/locales/ta/translaton.json";
+import hi from "./i18n/locales/hi/translaton.json";
+import fr from "./i18n/locales/fr/translaton.json";
+import es from "./i18n/locales/es/translaton.json";
+const messages = { en, fr, ar, hi, ta,es };
+
+export default function App() {
+  const [locale, setLocale] = useState("en");
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      
+      <select
+        value={locale}
+        onChange={(e) => setLocale(e.target.value)}
+      >
+        <option value="en">English</option>
+        <option value="fr">French</option>
+        <option value="es">Spanish</option>
+        <option value="hi">Hindi</option>
+        <option value="ta">Tamil</option>
+      </select>
+
+     const [locale, setLocale] = useState("en");
+
+  
+
+  <FoodStory locale ={locale} />
+</IntlProvider>
+    
+  );
+}
