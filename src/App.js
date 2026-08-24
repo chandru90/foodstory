e1160952ca1851dd94929090aa1be3c3bd4d9717
chandru90 <iochandru90@gmail.com>
2026-08-23 18:12:59 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import { IntlProvider } from "react-intl";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "./Navbar.js";
