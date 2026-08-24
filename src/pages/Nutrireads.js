0b10bd4c9f623d865773c83be5d5f061259451b2
chandru90 <iochandru90@gmail.com>
2026-08-23 19:53:16 -0700

    update

--- src/pages/Nutrireads.js
+++ src/pages/Nutrireads.js
@@ -321,7 +321,7 @@ const Nutriread =()=>{
 
         // No cache → request API
         const response = await axios.get(
-          "http://localhost:3006/linkedin"
+          "https://recipe-wnrc.onrender.com/linkedin"
         );
 
         const data = response.data.posts;
